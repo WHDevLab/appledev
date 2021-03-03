51e0ff727f619f5a4061491cb3bf8c859c2b911f
qp <>
2021-03-03 16:29:01 +0800

    111

--- appledev/app.py
+++ appledev/app.py
@@ -5,42 +5,17 @@ from datetime import datetime, timedelta
 import time
 import json
 import base64
-
-ALGORITHM = 'ES256'
+from auth import Auth
+import utils
 BASE_API = "https://api.appstoreconnect.apple.com"
 
 
 class AppStoreConnect:
 	def __init__(self, key_id, key_file, issuer_id):
-		self._token = None
-		self.token_gen_date = None
-		self.exp = None
-		self.key_id = key_id
-		self.key_file = key_file
-		self.issuer_id = issuer_id
-		self._debug = False
-		token = self.token  # generate first token
-
-	@property
-	def token(self):
-		# generate a new token every 15 minutes
-		if not self._token or self.token_gen_date + timedelta(minutes=15) < datetime.now():
-			self._token = self._generate_token()
-
-		return self._token
-
-	def _generate_token(self):
-		key = open(self.key_file, 'r').read()
-		self.token_gen_date = datetime.now()
-		exp = int(time.mktime((self.token_gen_date + timedelta(minutes=20)).timetuple()))
-		return jwt.encode({'iss': self.issuer_id, 'exp': exp, 'aud': 'appstoreconnect-v1'}, key,
-		                   headers={'kid': self.key_id, 'typ': 'JWT'}, algorithm=ALGORITHM).decode('ascii')
-
+		self.auth = Auth(key_id, key_file, issuer_id)
 			
 	def _api_call(self, uri, method="get", post_data=None):
-		headers = {"Authorization": "Bearer %s" % self.token}
-		if self._debug:
-			print(uri)
+		headers = {"Authorization": "Bearer %s" % self.auth.get_token()}
 		r = {}
 
 		url = BASE_API+uri
@@ -59,7 +34,6 @@ class AppStoreConnect:
 			payload = r.json()
 			return payload
 		elif content_type == 'application/a-gzip':
-			# TODO implement stream decompress
 			data_gz = b""
 			for chunk in r.iter_content(1024 * 1024):
 				if chunk:
@@ -74,10 +48,41 @@ class AppStoreConnect:
 	def fetch(self, uri, method="get", post_data=None):
 		return self._api_call(uri, method, post_data)
 		
+	def registerDevice(self, name, udid):
+		attributes = {"udid":udid, "name":name, "platform":"IOS"}
+		data = {"data":{"type":"devices", "attributes":attributes}}
+		response = self.fetch(uri="/v1/devices", method="post", post_data=data)
+		jsondata = utils.dump_response(response)
+		errors = jsondata["errors"]
+		if len(errors) == 0:
+			print("操作成功")
+		return jsondata
+
+	def getDevices(self):
+		response = self.fetch(uri="/v1/devices")
+		jsondata = utils.dump_response(response)
+		return jsondata
+
+	# IOS_APP_DEVELOPMENT, IOS_APP_STORE, IOS_APP_ADHOC, IOS_APP_INHOUSE, MAC_APP_DEVELOPMENT, MAC_APP_STORE, MAC_APP_DIRECT, TVOS_APP_DEVELOPMENT, TVOS_APP_STORE, TVOS_APP_ADHOC, TVOS_APP_INHOUSE, MAC_CATALYST_APP_DEVELOPMENT, MAC_CATALYST_APP_STORE, MAC_CATALYST_APP_DIRECT
+	def createProfile(self, name, bid, cids, devices):
+		attributes = {"name":name, "profileType":"IOS_APP_ADHOC"}
+		bundleId = {"id":bid,"type":"bundleIds"}
+		certificates = []
+		for cid in cids:
+			certificates.append({"id":cid, "type":"certificates"})
+		
+		devices = []
+		for did in devices:
+			devices.append({"id":did, "type":"devices"})
+
+		relationships = {"bundleId":{"data":bundleId}, "certificates":{"data":certificates}, "devices":{"data":devices}}
+		data = {"data":{"type":"profiles", "attributes":attributes, "relationships":relationships}}
+		response = self.fetch(uri="/v1/bundleIds", method="post", post_data=data)
+		jsondata = utils.dump_response(response)
+		return jsondata
 
 	def getProfiles(self):
 		return self._api_call("/v1/profiles")
-
 
 	def downloadProfile(self, profileID=None, saveFolderPath=None):
 		try:
@@ -112,4 +117,3 @@ class AppStoreConnect:
 			return "success"
 		except FileNotFoundError:
 			return "failure"
-
